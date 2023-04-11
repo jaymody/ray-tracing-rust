d84f12516eae9272f705d3d15b97a8c2a245e458
jaymody <jaykmody@gmail.com>
2023-04-10 20:41:17 -0400

    Completed chapter 4.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,10 @@
+mod ray;
 mod vec3;
 
+use ray::Ray;
 use vec3::Vec3;
 
-fn write_color(v: Vec3) {
-    println!(
-        "{} {} {}",
-        (v.x() * 255.999) as u32,
-        (v.y() * 255.999) as u32,
-        (v.z() * 255.999) as u32
-    );
-}
-
-fn main() {
-    const IMAGE_WIDTH: u32 = 256;
-    const IMAGE_HEIGHT: u32 = 256;
-
-    println!("P3");
-    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
-    println!("255");
-
-    for j in (0..IMAGE_HEIGHT).rev() {
-        eprint!("\rScanlines remaining {j}");
-        for i in 0..IMAGE_WIDTH {
-            let rgb: Vec3 = Vec3::new(
-                i as f64 / (IMAGE_WIDTH - 1) as f64,
-                j as f64 / (IMAGE_HEIGHT - 1) as f64,
-                0.25,
-            );
-            write_color(rgb);
-        }
-    }
-    eprintln!("\nDone");
-
+fn test_vec3() {
     let u = Vec3::new(1.0, 2.0, 3.0);
     let v = Vec3::new(-2.0, 1.0, 2.0);
 
@@ -48,9 +21,79 @@ fn main() {
     eprintln!("{:>20}  =  {}", "u / v", u / v);
     eprintln!("{:>20}  =  {}", "u.squared_length()", u.squared_length());
     eprintln!("{:>20}  =  {}", "u.length()", u.length());
-    eprintln!("{:>20}  =  {}", "u.unit()", u.unit());
+    eprintln!("{:>20}  =  {}", "u.unit_vector()", u.unit_vector());
     eprintln!("{:>20}  =  {}", "u.dot(v)", u.dot(v));
     eprintln!("{:>20}  =  {}", "v.dot(u)", v.dot(u));
     eprintln!("{:>20}  =  {}", "u.cross(v)", u.cross(v));
     eprintln!("{:>20}  =  {}", "v.cross(u)", v.cross(u));
+}
+
+fn ray_color(r: Ray) -> Vec3 {
+    let unit_direction: Vec3 = r.direction.unit_vector();
+    let t: f64 = (unit_direction.y + 1.0) * 0.5;
+    Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
+}
+
+fn write_color(v: Vec3) {
+    println!(
+        "{} {} {}",
+        (v.x * 255.999) as u32,
+        (v.y * 255.999) as u32,
+        (v.z * 255.999) as u32
+    );
+}
+
+fn main() {
+    test_vec3();
+
+    // image
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u32 = 400;
+    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
+
+    // camera (eye)
+    const VIEWPORT_HEIGHT: f64 = 2.0;
+    const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+    const FOCAL_LENGTH: f64 = 1.0;
+    const ORIGIN: Vec3 = Vec3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+    const HORIZONTAL: Vec3 = Vec3 {
+        x: VIEWPORT_WIDTH,
+        y: 0.0,
+        z: 0.0,
+    };
+    const VERTICAL: Vec3 = Vec3 {
+        x: 0.0,
+        y: VIEWPORT_HEIGHT,
+        z: 0.0,
+    };
+    const DEPTH: Vec3 = Vec3 {
+        x: 0.0,
+        y: 0.0,
+        z: FOCAL_LENGTH,
+    };
+    let lower_left_corner: Vec3 = ORIGIN - HORIZONTAL / 2.0 - VERTICAL / 2.0 - DEPTH;
+
+    // render
+    eprintln!("\nStarting render");
+
+    println!("P3");
+    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining {j}");
+        for i in 0..IMAGE_WIDTH {
+            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let r: Ray = Ray::new(ORIGIN, lower_left_corner + HORIZONTAL * u + VERTICAL * v);
+            let pixel_color: Vec3 = ray_color(r);
+            write_color(pixel_color);
+        }
+    }
+
+    eprintln!("\nDone");
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -3,45 +3,33 @@ use std::ops::{Add, Div, Mul, Neg, Sub};
 
 #[derive(Copy, Clone)]
 pub struct Vec3 {
-    x: f64,
-    y: f64,
-    z: f64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
 impl Vec3 {
     pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
         Vec3 { x, y, z }
     }
-
-    pub fn x(self) -> f64 {
-        self.x
-    }
-    pub fn y(self) -> f64 {
-        self.y
-    }
-    pub fn z(self) -> f64 {
-        self.z
-    }
-
     pub fn squared_length(self) -> f64 {
         self.x * self.x + self.y * self.y + self.z * self.z
     }
     pub fn length(self) -> f64 {
         self.squared_length().sqrt()
     }
-    pub fn unit(self) -> Vec3 {
+    pub fn unit_vector(self) -> Vec3 {
         self / self.length()
     }
-
     pub fn dot(self, v: Vec3) -> f64 {
         self.x * v.x + self.y * v.y + self.z * v.z
     }
     pub fn cross(self, v: Vec3) -> Vec3 {
-        Vec3::new(
-            self.y * v.z - self.z * v.y,
-            self.z * v.x - self.x * v.z,
-            self.x * v.y - self.y * v.x,
-        )
+        Vec3 {
+            x: self.y * v.z - self.z * v.y,
+            y: self.z * v.x - self.x * v.z,
+            z: self.x * v.y - self.y * v.x,
+        }
     }
 }
 
@@ -56,7 +44,11 @@ impl fmt::Display for Vec3 {
 impl Neg for Vec3 {
     type Output = Vec3;
     fn neg(self) -> Vec3 {
-        Vec3::new(-self.x, -self.y, -self.z)
+        Vec3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
     }
 }
 
@@ -64,13 +56,21 @@ impl Neg for Vec3 {
 impl Add<Vec3> for Vec3 {
     type Output = Vec3;
     fn add(self, v: Vec3) -> Vec3 {
-        Vec3::new(self.x + v.x, self.y + v.y, self.z + v.z)
+        Vec3 {
+            x: self.x + v.x,
+            y: self.y + v.y,
+            z: self.z + v.z,
+        }
     }
 }
 impl Add<f64> for Vec3 {
     type Output = Vec3;
     fn add(self, f: f64) -> Vec3 {
-        Vec3::new(self.x + f, self.y + f, self.z + f)
+        Vec3 {
+            x: self.x + f,
+            y: self.y + f,
+            z: self.z + f,
+        }
     }
 }
 
@@ -78,13 +78,21 @@ impl Add<f64> for Vec3 {
 impl Sub<Vec3> for Vec3 {
     type Output = Vec3;
     fn sub(self, v: Vec3) -> Vec3 {
-        Vec3::new(self.x - v.x, self.y - v.y, self.z - v.z)
+        Vec3 {
+            x: self.x - v.x,
+            y: self.y - v.y,
+            z: self.z - v.z,
+        }
     }
 }
 impl Sub<f64> for Vec3 {
     type Output = Vec3;
     fn sub(self, f: f64) -> Vec3 {
-        Vec3::new(self.x - f, self.y - f, self.z - f)
+        Vec3 {
+            x: self.x - f,
+            y: self.y - f,
+            z: self.z - f,
+        }
     }
 }
 
@@ -92,13 +100,21 @@ impl Sub<f64> for Vec3 {
 impl Mul<Vec3> for Vec3 {
     type Output = Vec3;
     fn mul(self, v: Vec3) -> Vec3 {
-        Vec3::new(self.x * v.x, self.y * v.y, self.z * v.z)
+        Vec3 {
+            x: self.x * v.x,
+            y: self.y * v.y,
+            z: self.z * v.z,
+        }
     }
 }
 impl Mul<f64> for Vec3 {
     type Output = Vec3;
     fn mul(self, f: f64) -> Vec3 {
-        Vec3::new(self.x * f, self.y * f, self.z * f)
+        Vec3 {
+            x: self.x * f,
+            y: self.y * f,
+            z: self.z * f,
+        }
     }
 }
 
@@ -106,12 +122,20 @@ impl Mul<f64> for Vec3 {
 impl Div<Vec3> for Vec3 {
     type Output = Vec3;
     fn div(self, v: Vec3) -> Vec3 {
-        Vec3::new(self.x / v.x, self.y / v.y, self.z / v.z)
+        Vec3 {
+            x: self.x / v.x,
+            y: self.y / v.y,
+            z: self.z / v.z,
+        }
     }
 }
 impl Div<f64> for Vec3 {
     type Output = Vec3;
     fn div(self, f: f64) -> Vec3 {
-        Vec3::new(self.x / f, self.y / f, self.z / f)
+        Vec3 {
+            x: self.x / f,
+            y: self.y / f,
+            z: self.z / f,
+        }
     }
 }
