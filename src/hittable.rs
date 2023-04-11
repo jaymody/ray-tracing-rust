48404540fd926e76cdbf6743bfa5e2d25a258854
jaymody <jaykmody@gmail.com>
2023-04-11 17:03:22 -0400

    Completed chapter 6.4.

--- src/hittable.rs
+++ src/hittable.rs
@@ -5,6 +5,7 @@ pub struct HitRecord {
     pub p: Vec3,
     pub normal: Vec3,
     pub t: f64,
+    pub front_face: bool,
 }
 
 impl HitRecord {
@@ -13,7 +14,17 @@ impl HitRecord {
             p: Vec3::ZERO,
             normal: Vec3::ZERO,
             t: 0.0,
+            front_face: false,
         }
+    }
+
+    pub fn set_face_normal(&mut self, ray: Ray, outward_normal: Vec3) {
+        self.front_face = outward_normal.dot(ray.direction) < 0.0;
+        self.normal = if self.front_face {
+            outward_normal
+        } else {
+            -outward_normal
+        };
     }
 }
 
